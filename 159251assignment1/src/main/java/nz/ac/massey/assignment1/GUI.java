c8bfa82b472b986aaa51900f50b1b11466b71580
MRY <sfls123@163.com>
2019-10-09 20:17:12 +0800

    add comment to GUI.java

--- 159251assignment1/src/main/java/nz/ac/massey/assignment1/GUI.java
+++ 159251assignment1/src/main/java/nz/ac/massey/assignment1/GUI.java
@@ -3,13 +3,15 @@ import java.awt.*;
 import javax.swing.*;
 public class GUI {
 	public static JFrame frmte;
+	//create new frame
 	JMenuBar jmb;	
 	JMenu menu1, menu2, menu3, menu4, menu5;
 	JMenuItem item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11;
 	JMenu open;	
 	JMenuItem txtfile, odtfile;
+	//create the whole menu
 	JTextArea jta;
-	
+	//create a text area for writing text 
 
 	public GUI() {
 		frmte = new JFrame();
@@ -17,16 +19,16 @@ public class GUI {
 		frmte.setBounds(600, 300, 500, 400);
 		frmte.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmte.setVisible(true);
-
+		//design the window parameter
 		
 		menu1 = new JMenu("File");
 		menu2 = new JMenu("Search");
 		menu3 = new JMenu("View");
 		menu4 = new JMenu("Manage");
 		menu5 = new JMenu("Help");		
-		
+		//set the name of menu
 		open = new JMenu("Open");		
-		
+		//set the name of the second menu
 		txtfile = new JMenuItem("txt");
 		odtfile = new JMenuItem("odt");
 		item1 = new JMenuItem("New");
@@ -40,7 +42,7 @@ public class GUI {
 		item9 = new JMenuItem("GetT&D");
 		item10 = new JMenuItem("About");
 		item11 = new JMenuItem("PDFconversation");
-		
+		//set name of menu item
 		open.add(txtfile);
 		open.add(odtfile);		
 		menu1.add(item1);
@@ -55,23 +57,24 @@ public class GUI {
 		menu4.add(item5);
 		menu4.add(item6);
 		menu5.add(item10);
-
+		//add second menu and items to menus respectively
 		jmb = new JMenuBar();
 		jmb.add(menu1);
 		jmb.add(menu2);
 		jmb.add(menu3);
 		jmb.add(menu4);
 		jmb.add(menu5);
-		
+		//add menu to bar
 		jta = new JTextArea();
 		JScrollPane jsp = new JScrollPane(jta);
 		jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		
 		frmte.setJMenuBar(jmb);	
 		frmte.add(jsp, BorderLayout.CENTER);	
-		
+		//add components to frame
 		
 		frmte.setVisible(true);
+		//set the window can be see
 	}
 	public static void main(String[] args) {
 		GUI texteditor = new GUI();
