d4c8fdd78c2074ed6f3016dab1fe53300921157b
Wang Yaoxiang <540900179@qq.com>
2019-10-11 23:38:04 +0800

    WYX

--- 159251assignment1/src/main/java/nz/ac/massey/assignment1/SCPC.java
+++ 159251assignment1/src/main/java/nz/ac/massey/assignment1/SCPC.java
@@ -1,19 +1,28 @@
 package nz.ac.massey.assignment1;
 
+import javax.swing.JTextArea;
+
 public class SCPC {
-	public void select() {
+	public void select(JTextArea jta) {
 		//TODO function Select
+		jta.selectAll();
+		
+		
 	}
 	
-	public void paste() {
+	public void paste(JTextArea jta) {
 		//TODO paste function
+		jta.paste();
+		
 	}
 	
-	public void copy() {
+	public void copy(JTextArea jta) {
 		//TODO copy function
+		jta.copy();
 	}
 	
-	public void cut() {
+	public void cut(JTextArea jta) {
 		//TODO cut function
+		jta.cut();
 	}
 }
